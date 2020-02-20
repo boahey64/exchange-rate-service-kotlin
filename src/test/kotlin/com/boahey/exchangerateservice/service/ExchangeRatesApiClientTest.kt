92aa6b343900b21b1fea821eeb91a0b84a9ea079
roman <roman.richter.git.user>
2020-02-20 14:25:20 +0100

    Use WiremockPule instead of WiremockServer

--- src/test/kotlin/com/boahey/exchangerateservice/service/ExchangeRatesApiClientTest.kt
+++ src/test/kotlin/com/boahey/exchangerateservice/service/ExchangeRatesApiClientTest.kt
@@ -16,33 +16,27 @@ import reactor.test.StepVerifier
 
 class ExchangeRatesApiClientTest {
 
-//    @get:Rule
-//    val wireMockRule = WireMockRule(WireMockConfiguration.wireMockConfig().dynamicPort())
-
     private lateinit var serviceUnderTest: ExchangeRatesApiClient
 
-
     companion object {
-        private val WIREMOCK_SERVER = WireMockServer(WireMockConfiguration.wireMockConfig().dynamicPort())
+        @get:Rule
+        val wireMockRule = WireMockRule(WireMockConfiguration.wireMockConfig().dynamicPort())
         @BeforeAll
         @JvmStatic
         fun beforeAll() {
-            WIREMOCK_SERVER.start()
+            wireMockRule.start()
         }
         @AfterAll
         @JvmStatic
         fun afterAll() {
-            WIREMOCK_SERVER.stop()
+            wireMockRule.stop()
         }
     }
-
-
 
     @BeforeEach
     fun setUp() {
         HttpResources.reset()
-//        serviceUnderTest = ExchangeRatesApiClient("http://localhost:${wireMockRule.port()}" );
-        serviceUnderTest = ExchangeRatesApiClient("http://localhost:${WIREMOCK_SERVER.port()}" );
+        serviceUnderTest = ExchangeRatesApiClient("http://localhost:${Companion.wireMockRule.port()}" );
     }
 
     @Test
@@ -78,7 +72,7 @@ class ExchangeRatesApiClientTest {
     }
 
     private fun aResponseWithStatusCode(httpStatus: Int, delay: Int, date: String, base: String) {
-        WIREMOCK_SERVER.stubFor(WireMock.get(WireMock.urlPathMatching("/$date"))
+        wireMockRule.stubFor(WireMock.get(WireMock.urlPathMatching("/$date"))
                 .withQueryParam("base", WireMock.equalTo(base))
                 .willReturn(WireMock.aResponse()
                         .withStatus(httpStatus)
