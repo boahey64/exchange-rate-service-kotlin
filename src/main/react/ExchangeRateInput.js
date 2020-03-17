7062561f0024fd1ed4349304e0b69180aa9539ea
roman <roman.richter.git.user>
2020-03-17 13:41:09 +0100

    added form tag

--- src/main/react/ExchangeRateInput.js
+++ src/main/react/ExchangeRateInput.js
@@ -3,98 +3,75 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchExchangeRate } from "./action"
-import styled from "styled-components";
-import colors from "./util/colors";
-
-type ItemProps = {
-    onCalculateExchangeRate: () => void
-};
-
-const StyledInput = styled.input`
-    appearance: none;
-    box-sizing: border-box;
-    border: 1px solid ${colors.grey_300};
-    border-radius: 0;
-    font-size: 16px;
-    height: 40px;
-    margin-bottom: 0;
-    padding: 10px;
-    width: 30%;
-
-    &:focus {
-        outline: none;
-        padding: 10px 65px 10px 10px;
-    }
-
-    @media screen and (min-width: 960px) {
-        font-size: 14px;
-    }
-
-    // avoid browsers native clear X
-    &::-ms-clear,
-    &::-ms-reveal {
-        display: none;
-        height: 0;
-        width: 0;
-    }
-    &::-webkit-search-decoration,
-    &::-webkit-search-cancel-button,
-    &::-webkit-search-results-button,
-    &::-webkit-search-results-decoration {
-        display: none;
-    }
-`;
-
 
 class ExchangeRateInput extends React.Component {
-    componentDidMount() {
-        this.props.dispatch(fetchExchangeRate());
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      baseCurrency: "EUR",
+      targetCurrency: "USD",
+      date: "2019-12-24"
+    };
 
-    handleButtonClick(param) {
-        console.log("handleButtonClick: " + param);
-    }
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-    render() {
-        const { error, loading } = this.props;
-        console.log("props: " + this.props.item);
-        if (error) {
-            return <div>Error! {error.message}</div>;
-        }
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+    console.log('name: {}', name);
 
-        if (loading) {
-            return <div>Loading...</div>;
-        }
+    this.setState({
+      [name]: value
+    });
+    console.log('name: ' + name  + ' value: ' + value);
+  }
 
-        const exchangeRate = this.props.item;
-        console.log("exchangeRate: " + exchangeRate);
-//                        <StyledInput
-//                            autoComplete="off"
-//                            spellCheck={false}
-//                            type="search"
-//                            value={props.currentFilterTerm}
-//                            onChange={props.onSearch}
-//                            smallScreen={props.smallScreen}
-//                            data-e2e-search
-//                        />
-//
-        return (
-                    <div className="input-field">
-                        <StyledInput
-                            autoComplete="off"
-                        />
-                        <StyledInput
-                            autoComplete="off"
-                        />
-                        <StyledInput
-                            autoComplete="off"
-                        />
-                        <div>
-                            <button onClick={() => this.handleButtonClick("column")}>submit</button>
-                        </div>
-                    </div>
-        );
-    }
+  handleSubmit(event) {
+    console.log('handleSubmit:');
+//    alert('Base Currency is: ' + this.state.baseCurrency);
+//    console.log('baseCurrency: {}', this.state.baseCurrency);
+    console.log(' baseCurrency: ' + this.state.baseCurrency);
+    console.log(' targetCurrency: ' + this.state.targetCurrency);
+    console.log(' date: ' + this.state.date);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+        <div>
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Base Currency:
+              <input
+                name="baseCurrency"
+                value={this.state.baseCurrency}
+                onChange={this.handleInputChange} />
+            </label>
+            <br />
+            <label>
+              Target Currency:
+              <input
+                name="targetCurrency"
+                value={this.state.targetCurrency}
+                onChange={this.handleInputChange} />
+            </label>
+            <br />
+            <label>
+              Date:
+              <input
+                name="date"
+                value={this.state.date}
+                onChange={this.handleInputChange} />
+            </label>
+            <br />
+            <input type="submit" value="Absenden" />
+          </form>
+        </div>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
